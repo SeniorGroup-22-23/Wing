10e440764d9e840da1328e0c87125be4732c5f96
alliegriffinn <agriffi1@unb.ca>
2022-10-15 13:16:53 -0300

    add test

--- Backend/Wing/Tests/AppTests/AppTests.swift
+++ Backend/Wing/Tests/AppTests/AppTests.swift
@@ -2,14 +2,14 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
+    
+    func testJson() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello", afterResponse: { res in
+        try app.test(.GET, "json", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
 }
