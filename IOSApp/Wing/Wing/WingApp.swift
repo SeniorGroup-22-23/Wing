39ec262e11eac72aeea2a6957a32183dda954f11
Nury Kim <75460797+itsnury@users.noreply.github.com>
2022-10-17 18:19:12 -0300

    Project Structure for Front-end

---
+++ IOSApp/Wing/Wing/WingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WingApp.swift
+//  Wing
+//
+//  Created by Allie Griffin on 2022-10-03.
+//
+
+import SwiftUI
+
+@main
+struct WingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomePageView()
+        }
+    }
+}
