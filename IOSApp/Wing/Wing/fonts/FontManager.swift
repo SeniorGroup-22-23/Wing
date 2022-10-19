e0892baf99ff7dfa23adf6da0a7addb0bddd170b
Nury Kim <75460797+itsnury@users.noreply.github.com>
2022-10-19 15:20:40 -0300

    Add files via upload

---
+++ IOSApp/Wing/Wing/fonts/FontManager.swift
@@ -0,0 +1,25 @@
+//
+//  FontManager.swift
+//  Wing
+//
+//  Created by Nury Kim on 2022-10-18.
+//
+
+import SwiftUI
+
+struct FontManager {
+    struct NotoSans {
+        static let regular = "NotoSans-Regular"
+        static let bold = "NotoSans-Bold"
+        static let light = "NotoSans-Light"
+        static let semiBold = "NotoSans-SemiBold"
+        static let italic = "NotoSans-Italic"
+    }
+    
+    struct KumbhSans {
+        static let regular = "KumbhSans-Regular"
+        static let bold = "KumbhSans-Bold"
+        static let light = "KumbhSans-Light"
+        static let semiBold = "KumbhSans-SemiBold"
+    }
+}
