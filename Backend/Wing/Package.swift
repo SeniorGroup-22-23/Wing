f95ec78abdc5bd6dc015678580ab47c2898589a4
Courtney P <77448773+cperrea1@users.noreply.github.com>
2022-11-03 16:52:15 -0300

    adding models

--- Backend/Wing/Package.swift
+++ Backend/Wing/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "Models", package: "models")
+                .product(name: "Models", package: "models"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
