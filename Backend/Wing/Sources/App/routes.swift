42f297fe9d1d2125b789e43d40c2f20d2f5a1fe2
Allie Griffin <80468156+alliegriffinn@users.noreply.github.com>
2022-11-08 18:16:23 -0400

    Update routes.swift
    Adding preliminary routes and custom error enum . 
    TODO add unit tests

--- Backend/Wing/Sources/App/routes.swift
+++ Backend/Wing/Sources/App/routes.swift
@@ -1,37 +1,87 @@
-import Fluent
+import FluentPostgresDriver
 import Vapor
+import Models
 
+//Wrap Abort Error to Generate Custom HTTP Errors
+enum Error {
+    case userNotFound
+}
+
+extension Error: AbortError {
+    var reason: String {
+        switch self {
+        case .userNotFound:
+            return "No user record found."
+        }
+    }
+    var status: HTTPStatus {
+        switch self {
+        case .userNotFound:
+            return .notFound
+        }
+    }
+}
+
+//Wing Routes
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    
+    //GET Usernames
+    app.get("usernames", ":usernameMatch") {  req async throws -> [String] in
+        let charMatch = req.parameters.get("usernameMatch")! //! forces decode to string, if empty no error
+        return try await User.query(on: req.db)
+            .filter(\.$username =~ charMatch)
+            .sort(\.$username)
+            .all(\.$username) //returns username field from all matches
     }
     
-    //CREATE
-    //map directly from request body content 
-    app.post("users"){ req -> EventLoopFuture<User> in
+    //GET User by Phone - DONE
+    app.get("user", "phone", ":phone", ":password") { req async throws -> User in
+        let phoneNum = req.parameters.get("phone")!
+        let password = req.parameters.get("password")!
+        guard let user = try await User.query(on: req.db)
+            .filter(\.$phone == phoneNum)
+            .filter(\.$password == password)
+            .first() //Will throw error if no User is found
+        else {
+            throw Error.userNotFound
+        }
+        return user
+    }
+    
+    //GET User by Email - DONE
+    app.get("user", "email", ":email", ":password") { req async throws -> User in
+        let email = req.parameters.get("email")!
+        let password = req.parameters.get("password")!
+        guard let user = try await User.query(on: req.db)
+            .filter(\.$email == email)
+            .filter(\.$password == password)
+            .first() //Will throw error if no User is found
+        else {
+            throw Error.userNotFound
+        }
+        return user
+    }
+    
+    //POST User
+    app.post("user"){ req -> EventLoopFuture<User> in
         let user = try req.content.decode(User.self)
         return user.create(on: req.db)
-            .map { user }
+            .map {user}
+    }
+    
+    //GET Prompts --DONE
+    app.get("prompts") { req async throws in
+        try await Prompt.query(on: req.db).all()
+    }
+    
+    //POST Prompt Response to user account -- DONE
+    app.post("prompts", "user"){ req -> EventLoopFuture<PromptResponse> in
+        let promptResponse = try req.content.decode(PromptResponse.self)
+        return promptResponse.create(on: req.db)
+            .map { promptResponse }
     }
     
     
-    //GET
-    app.get("users") { req async throws in
-        try await User.query(on: req.db).all()
-    }
-    
-    app.get("json") { req in
-        return Person(name: "Allie M. Griffin", age: 26)
-    }
-    
-
-    app.get("test_ci"){ req in
-        return "Testing CI Pipeline again lol"
-    }
-    //try app.register(collection: TodoController())
+    //try app.register(collection: TodoController()) XCODE generated
 }
 
-struct Person: Content {
-    var name: String
-    var age: Int
-}
