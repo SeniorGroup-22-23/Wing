155780d09db3ff2a60369d5bde5d46889a655434
Nury Kim <nkim@unb.ca>
2022-11-15 21:08:57 -0400

    just the bare bones

---
+++ IOSApp/Wing/Wing/views/AddPhotosPageView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPhotosPageView.swift
+//  Wing
+//
+//  Created by Nury Kim on 2022-11-15.
+//
+
+import SwiftUI
+
+struct AddPhotosPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPhotosPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPhotosPageView()
+    }
+}
