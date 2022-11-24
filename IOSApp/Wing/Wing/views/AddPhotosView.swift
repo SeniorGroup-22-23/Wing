4f568f701450ecaad0ea975d9752d7a08cbb88d2
Nury Kim <nkim@unb.ca>
2022-11-23 22:24:35 -0400

    logic finally works

--- IOSApp/Wing/Wing/views/AddPhotosView.swift
+++ IOSApp/Wing/Wing/views/AddPhotosView.swift
@@ -12,77 +12,84 @@ struct AddPhotosView: View {
     @State private var arr = [0,0,2,4,6]
     @State private var showingImagePicker = false
     
-    @State private var images = [Image?]()
+    @State private var images = [Image?](repeating : nil, count : 8)
     @State private var inputImage: UIImage?
     
     var body: some View {
-        ZStack {
-            Color("White")
-            
-            VStack{
-                VStack {
-                    loadLogo()
-                        .frame(width : 120.0, height : 127.0)
-                    
-                    HStack {
-                        loadUploadPhotosText()
-                    }
-                        .padding(.top, -20)
-                    
-                    HStack {
-                        loadMinimumPhotosText()
-                            .frame(maxWidth: 300.0, alignment: .leading)
-                    }
-                    
-                    HStack {
-                        loadResetPhotosBtn()
-                            .frame(maxWidth: 300.0, alignment: .leading)
-                    }
-                        .padding(.bottom, 10)
-                }
-                .padding(.top, -30)
+        NavigationView {
+            ZStack {
+                Color("White")
                 
-                ScrollView {
-                    VStack(spacing : 10) {
-                        ForEach(1..<5) { i in
-                            HStack(spacing : 10) {
-                                ForEach(1..<3) { j in
-                                    Group {
-                                        if (arr[i] + j == numPhotos + 1) {
-                                            ZStack {
-                                                loadEmptyPhotoBox()
-                                                loadPlusSignText()
-                                            }
+                VStack{
+                    VStack {
+                        loadLogo()
+                            .frame(width : 120.0, height : 127.0)
+                        
+                        HStack {
+                            loadUploadPhotosText()
+                        }
+                        .padding(.top, -20)
+                        
+                        HStack {
+                            loadMinimumPhotosText()
+                                .frame(maxWidth: 300.0, alignment: .leading)
+                        }
+                        
+                        HStack {
+                            loadResetPhotosBtn()
+                                .frame(maxWidth: 300.0, alignment: .leading)
+                        }
+                        .padding(.bottom, 10)
+                    }
+                    .padding(.top, -30)
+                    
+                    ScrollView {
+                        VStack(spacing : 10) {
+                            ForEach(1..<5) { i in
+                                HStack(spacing : 10) {
+                                    ForEach(1..<3) { j in
+                                        Group {
+                                            let index = arr[i] + j
+                                            if (index <= numPhotos) {
+                                                getImage(num: index)
+                                                    .frame(width: 140, height: 175)
+                                                    .cornerRadius(10)
+                                            } else if (index == numPhotos + 1) {
+                                                ZStack {
+                                                    loadEmptyPhotoBox()
+                                                    loadPlusSignText()
+                                                }
                                                 .onTapGesture {
                                                     showingImagePicker = true
                                                 }
-                                                .onChange(of: inputImage) { _ in loadImage() }
-                                                .sheet(isPresented: $showingImagePicker) {
-                                                    ImagePicker(image: $inputImage)
-                                                }
-                                        } else {
-                                            loadEmptyPhotoBox()
+                                            } else {
+                                                loadEmptyPhotoBox()
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                        .onChange(of: inputImage) { _ in loadImage() }
+                        .sheet(isPresented: $showingImagePicker) {
+                            ImagePicker(image: $inputImage)
+                        }
+                    
+                    Spacer()
+                    
+                    loadPhotoSuggesionText()
+                        .multilineTextAlignment(.center)
+                    
+                    NavigationLink(destination: HomePageView()) {
+                        loadDoneText()
+                            .frame(width: 231.0, height: 55.0)
+                            .background(Color("MainGreen"))
+                            .cornerRadius(20)
+                    }
                 }
-                
-                Spacer()
-                
-                loadPhotoSuggesionText()
-                    .multilineTextAlignment(.center)
-                
-                NavigationLink(destination: HomePageView()) {
-                    loadDoneText()
-                        .frame(width: 231.0, height: 55.0)
-                        .background(Color("MainGreen"))
-                        .cornerRadius(20)
-                }
+                .frame(width: 400.0)
             }
-            .frame(width: 400.0)
         }
     }
     
@@ -129,13 +136,18 @@ struct AddPhotosView: View {
     
     func loadResetPhotosBtn () -> some View {
         return Button("Reset Photos") {
-            // TODO : reset all images to nothing
+            numPhotos = 0
         }
             .frame(width: 120.0, height: 30.0)
             .background(Color("DarkGrey"))
             .cornerRadius(20)
             .foregroundColor(.white)
             .font(.custom("KumbhSans", size: 16.0))
+    }
+    
+    func getImage(num : Int) -> Image? {
+        return images[num - 1]?
+            .resizable()
     }
     
     func loadImage() {
