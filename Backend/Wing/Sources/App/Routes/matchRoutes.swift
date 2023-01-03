124130930bc49aa50d4181d9d3e7eb3aa325965e
alliegriffinn <agriffi1@unb.ca>
2023-01-03 13:09:57 -0400

    Finalize match routes

--- Backend/Wing/Sources/App/Routes/matchRoutes.swift
+++ Backend/Wing/Sources/App/Routes/matchRoutes.swift
@@ -17,11 +17,10 @@ func matchRoutes(_ app: Application) throws {
     decoder.dateDecodingStrategy = .iso8601
     
     //Create Swipe Record
-    //returns true if match was found, false otherwise 
     app.post("swipe"){ req async throws -> Bool in
-        let temp = try req.content.decode(Swipe.self)
-        let lowerSId = UUID(String(temp.swiperId).lowercased())!
-        let lowerPId = UUID(String(temp.prospectId).lowercased())!
+        let currRecord = try req.content.decode(Swipe.self)
+        let lowerSId = UUID(String(currRecord.swiperId).lowercased())!
+        let lowerPId = UUID(String(currRecord.prospectId).lowercased())!
         
         let existingRecord = try await Swipe.query(on: req.db) //see if any record exists where current prospect has swiped on current swiper
                 .filter(\.$swiperId == lowerPId)
@@ -31,7 +30,7 @@ func matchRoutes(_ app: Application) throws {
             if(existingRecord == nil){ //no record exists, create one
                 let record = Swipe(swiperId: lowerSId, //force lowercase IDs for consistency
                                    prospectId: lowerPId,
-                                   type: temp.type,
+                                   type: currRecord.type,
                                    createdAt: Date())
                 try await record.create(on: req.db)
                 return false //no match
@@ -41,17 +40,16 @@ func matchRoutes(_ app: Application) throws {
             let lowerId = UUID(String(id!).lowercased())!
             
             //a record did exist between the two users
-            if(existingRecord?.type != 2 && temp.type != 2){
-                //both users swiped yes (on thier own or through wing) create a match record and ?delete swipe record?
+            if(existingRecord?.type != 2 && currRecord.type != 2){
+                //both users swiped yes (on thier own or through wing) create a match record and delete swipe record
                 let matchRecord: Match
-                if(existingRecord?.type == 3 || temp.type == 3){
+                if(existingRecord?.type == 3 || currRecord.type == 3){
                     matchRecord = Match(firstUserId: lowerSId, secondUserId: lowerPId, type: 2) //via wing
                 } else {
                     matchRecord = Match(firstUserId: lowerSId, secondUserId: lowerPId, type: 1) //direct
                 }
                 try await matchRecord.create(on: req.db)
                 
-                //TODO Remove this?
                 try await Swipe.query(on: req.db)
                     .filter(\.$id == lowerId)
                     .delete()
@@ -59,11 +57,14 @@ func matchRoutes(_ app: Application) throws {
                 return true
                 
             } else {
+                
                 //update existing record to a dislike
                 try await Swipe.query(on: req.db)
                     .set(\.$type, to: 2)
                     .filter(\.$id == lowerId)
                     .update()
+                
+                return false
             }
             
         } catch {
@@ -75,36 +76,36 @@ func matchRoutes(_ app: Application) throws {
                 throw Abort(.internalServerError, reason: "Unable to update record: \(error)")
             }
         }
-        
-        return false
     }
     
     //unmatch given both user IDs
-    //TODO: TEST and Document
     app.post("unmatch",":id1",":id2"){ req async throws -> Response in
         //delete match record
-        let id1 = UUID(req.parameters.get("id1")!.lowercased())
-        let id2 = UUID(req.parameters.get("id2")!.lowercased())
-        
-        do {
-            try await Match.query(on: req.db)
-                .filter(\.$firstUserId == id1!)
-                .filter(\.$secondUserId == id2!)
-                .delete()
-            
-            try await Match.query(on: req.db)
-                .filter(\.$firstUserId == id2!)
-                .filter(\.$secondUserId == id1!)
-                .delete()
-        } catch {
+        guard let id1 = UUID(req.parameters.get("id1")!.lowercased())
+        else{
             throw Error.nilId
         }
+        
+        guard let id2 = UUID(req.parameters.get("id2")!.lowercased())
+        else{
+            throw Error.nilId
+        }
+    
+        try await Match.query(on: req.db)
+            .filter(\.$firstUserId == id1)
+            .filter(\.$secondUserId == id2)
+            .delete()
+        
+        try await Match.query(on: req.db)
+            .filter(\.$firstUserId == id2)
+            .filter(\.$secondUserId == id1)
+            .delete()
+        
         return Response(status: .accepted)
     }
     
     
     //get all prospects profileIDs
-    //TODO: TEST and document
     app.get("prospects", ":userId"){ req async throws -> [UUID] in
         guard let userId = UUID(req.parameters.get("userId")!.lowercased())
         else {
@@ -112,7 +113,7 @@ func matchRoutes(_ app: Application) throws {
         }
         
         //get profile of current user
-        guard let profile = try await Profile.query(on: req.db)
+        guard let swiperProfile = try await Profile.query(on: req.db)
             .filter(\.$userId == userId)
             .first() //Will throw error if no User is found
         else {
@@ -120,7 +121,7 @@ func matchRoutes(_ app: Application) throws {
         }
         
         //get invalid userIDs
-        var invalids = [userId]
+        var invalids = [userId] //initally just current swiper is invalid
         invalids += try await Swipe.query(on: req.db) //currently in swipes
             .filter(\.$swiperId == userId)
             .all(\.$prospectId)
@@ -137,25 +138,26 @@ func matchRoutes(_ app: Application) throws {
             .filter(\.$secondUserId == userId)
             .all(\.$firstUserId)
         
-        let prospects: [UUID]
-        let maxBirthdate = userCalendar.date(byAdding: .year, value: -Int(profile.minAge), to: Date())!
-        let minBirthdate = userCalendar.date(byAdding: .year, value: -Int(profile.maxAge), to: Date())!
+        let prospects: [UUID] //initially empty array
+        let maxBirthdate = userCalendar.date(byAdding: .year, value: -Int(swiperProfile.minAge), to: Date())!
+        let minBirthdate = userCalendar.date(byAdding: .year, value: -Int(swiperProfile.maxAge), to: Date())!
         
-        if(profile.preference == 3){
-            //do not check gender in req
-            prospects = try await Profile.query(on: req.db) //TODO: add location filter
+        if(swiperProfile.preference == 3){ //do not check gender in req (pref == any)
+            prospects = try await Profile.query(on: req.db)
                 .filter(\.$userId !~ invalids)
                 .filter(\.$birthdate <= maxBirthdate)
                 .filter(\.$birthdate >= minBirthdate)
                 .all(\.$id)
+               //TODO: add location filter
             
         } else {
-            prospects = try await Profile.query(on: req.db) //TODO: add location filter
+            prospects = try await Profile.query(on: req.db)
                 .filter(\.$userId !~ invalids)
-                .filter(\.$gender == profile.preference)
+                .filter(\.$gender == swiperProfile.preference)
                 .filter(\.$birthdate <= maxBirthdate)
                 .filter(\.$birthdate >= minBirthdate)
                 .all(\.$id)
+               //TODO: add location filter
         }
         
         return prospects //returns empty array if no prospects
