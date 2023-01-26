727f829f43dc7fa027ba7007724da0c9c258e9b3
Allie Griffin <80468156+alliegriffinn@users.noreply.github.com>
2023-01-26 17:47:18 -0400

    Update Package.swift

--- Backend/Wing/Package.swift
+++ Backend/Wing/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(path: "../../Models"),
+        .package(path: "https://github.com/SeniorGroup-22-23/Models"),
     ],
     targets: [
         .target(
