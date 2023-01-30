89550f0d5295d87c3579e9cfa50acc19d272f749
Courtney P <77448773+cperrea1@users.noreply.github.com>
2023-01-30 10:52:14 -0400

    updating Photo test

--- Models/Tests/ModelsTests/PhotoTests.swift
+++ Models/Tests/ModelsTests/PhotoTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 final class PhotoTests: XCTestCase {
     
-    func testInitProfilePreview() throws {
+    func testInitPhoto() throws {
         
         let userId = UUID()
         let photoData = Data()
