db71b26aed49f721123833165e292b2da3ab8e65
alliegriffinn <agriffi1@unb.ca>
2023-03-08 20:35:11 -0400

    change back to default HomePageView()

--- IOSApp/Wing/Wing/WingApp.swift
+++ IOSApp/Wing/Wing/WingApp.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct WingApp: App{
     var body: some Scene {
         WindowGroup {
-            //HomePageView()
-            MatchView()
+            HomePageView()
+            //MatchView()
         }
     }
 }
